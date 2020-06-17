582901ddb01b65ae0429d98c5d1045a94accf57f
AssessedPlate93 <62059915+AssessedPlate93@users.noreply.github.com>
2020-06-17 13:39:54 +0200

    Update Main.php

--- .github/Main.php
+++ .github/Main.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace assessed\Generator;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\level\generator\Generator;
+use pocketmine\level\generator\Generator; #Plugin is not finished. Please do not copy. When it will be aviable to use, i will share download link...
 
 class Main extends PluginBase{
 	public static function generate($path, $name, $seed, $generator, array $options = []){
 		@mkdir($path, 0777, true);
-		@mkdir($path, 0777);
+		@mkdir($path, 0777); #To-Do Find better method for that
 	}
 	public function onLoad(){
-        Generator::addGenerator(chunk::class,   chunk::chunk);
+        Generator::addGenerator(chunk::class,   chunk::chunk); #Downstairs-soon
+		
     }
 }
